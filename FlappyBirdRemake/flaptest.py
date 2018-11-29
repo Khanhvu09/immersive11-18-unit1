17f876f45b00b5245a14b4348b8db463d1d8b0b4
Brandon Hernandez <40280381+brandonhdez7@users.noreply.github.com>
2018-11-29 09:19:32 -0500

    Add files via upload

--- FlappyBirdRemake/flaptest.py
+++ FlappyBirdRemake/flaptest.py
@@ -3,6 +3,7 @@ import random
 import sys
 import pygame
 from random import randint
+from button import Start_Button
 
 black = (0,0,0)
 #white = (255,255,255)
@@ -21,6 +22,7 @@ pygame_screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('Flappy Bird')
 clock = pygame.time.Clock()
 
+start_button = Start_Button(pygame_screen)
 
 pipe_green = pygame.image.load('pipe-green.png')
 pipe_green_invert = pygame.image.load('pipe-green1.png')
@@ -52,12 +54,12 @@ background_day = pygame.image.load('background-day.png')
 def gameover():
     #font = pygame.font.SysFont(None,75)
     #text = font.render("Game Over", True, red)
-    pygame_screen.blit(gameover_image,[70,250])
+    pygame_screen.blit(gameover_image,[60,250])
 
 
 def obstacle(xloc, yloc, xsize, ysize):
     pygame.draw.rect(pygame_screen, green, [xloc, yloc, xsize, ysize])
-    pygame.draw.rect(pygame_screen, green, [xloc, int(yloc + ysize + space), xsize, ysize+500])
+    pygame.draw.rect(pygame_screen, green, [xloc, int(yloc+ysize+space), xsize, ysize+500])
     # pygame_screen.blit(pipe_green_invert, [xloc, -yloc])
     # pygame_screen.blit(pipe_green, [xloc, int(yloc + ysize + space)])
     
@@ -83,16 +85,18 @@ y = 250
 y_speed = 0
 x_speed = 0
 ground = 395
-xloc = 350
+xloc = 300
+yloc = 0
 xsize = 52
 ysize = randint(0,320)
 yloc = 0
-space = 150
+space = 125
 obspeed = 3
 score = 0
 
 
 game_on = True
+game_start = False
 
 while game_on:
     for event in pygame.event.get():
@@ -106,6 +110,16 @@ while game_on:
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_UP:
                 y_speed = 5
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            #print mouse_x,mouse_y
+            if start_button.rect.collidepoint(mouse_x, mouse_y):
+                game_start = True
+                #bg_music = pygame.mixer.Sound('faf.wav')
+                #bg_music.play()
+
+
     pygame_screen.blit(background_day,[0,0])
     #pygame_screen.blit(redbird_downflap_image,[200,200])
     obstacle(xloc, yloc, xsize, ysize)
@@ -113,36 +127,40 @@ while game_on:
     Score(score)
     pygame_screen.blit(base,[0,400])
 
+    if game_start:
     
-    
 
-    y += y_speed
-    xloc -= obspeed
+        y += y_speed
+        xloc -= obspeed
 
-    if y > ground:
-        game_over()
-        y_speed = 0
-        obspeed = 0
+        if y > ground:
+            game_over()
+            y_speed = 0
+            obspeed = 0
 
-    if x+20 > xloc and y-20 < ysize and x-15 < xsize+xloc:
-        gameover()
-        obspeed = 0
-        yspeed = 0
+        if x+20 > xloc and y-20 < ysize and x-15 < xsize+xloc:
+            gameover()
+            obspeed = 0
+            yspeed = 0
 
-    if x+20 > xloc and y+20 > ysize + space and x-15 < xsize+xloc:
-        gameover()
-        obspeed = 0
-        y_speed = 0
+        if x+20 > xloc and y+20 > ysize + space and x-15 < xsize+xloc:
+            gameover()
+            obspeed = 0
+            y_speed = 0
 
-    if xloc < -80:
-        xloc = 300
+        if xloc < -80:
+            xloc = 300
+            y_size = randint(0,350)
 
-        y_size = randint(0,100)
+        if x > xloc and x < xloc+3:
+            score = (score + 1)
 
-    if x > xloc and x < xloc+3:
-        score = (score + 1)
-        
+    if game_start == False:
+        start_button.setup_message()
+        start_button.draw_button()
+       
     pygame.display.flip()
     clock.tick(60)
+
     
 pygame.quit()
